e4d9aaf324c0b16f5eba89a61bffb89a31ec9739
Ashok Sah <ashok.stelmec@gmail.com>
2022-08-11 04:08:50 +0530

    Examples of Usee Context Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from "react";
+import  "./App.css";
+import ComA from "./ComA";
 
-function App() {
+const FirstName = createContext();
+const LastName = createContext();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FirstName.Provider value={"Ashok"}>
+        <LastName.Provider value={"Singh"}>
+        <ComA />
+        </LastName.Provider>
+      </FirstName.Provider>
+    </>
   );
-}
+};
 
 export default App;
+export { FirstName, LastName };
